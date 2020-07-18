c095193a7d20a7e3ecfdcf360344aeeee6baf6a5
耗子 <45513686+haoziz@users.noreply.github.com>
2020-07-19 00:07:45 +0800

    中文翻译

--- vendor/loco-translate/src/admin/list/PluginsController.php
+++ vendor/loco-translate/src/admin/list/PluginsController.php
@@ -9,7 +9,7 @@ class Loco_admin_list_PluginsController extends Loco_admin_list_BaseController {
     public function render(){
 
         $this->set( 'type', 'plugin' );
-        $this->set( 'title', __( 'Translate plugins', 'loco-translate' ) );
+        $this->set( 'title', __( '插件翻译', 'loco-translate' ) );
         
         foreach( Loco_package_Plugin::get_plugins() as $handle => $data ){
             try {
