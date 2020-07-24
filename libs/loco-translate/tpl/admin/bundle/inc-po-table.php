0b12cd73b66fd809b545fec49a244a25b0af8ca8
sunxiyuan <sxy@ibadboy.net>
2020-07-24 08:46:48 +0800

    refactor:修改翻译包未发现时的提示语

--- libs/loco-translate/tpl/admin/bundle/inc-po-table.php
+++ libs/loco-translate/tpl/admin/bundle/inc-po-table.php
@@ -18,6 +18,8 @@
         endforeach;
     endif;
 ?>
-
-该包还未被<a href="https://translate.wp-china.org">https://translate.wp-china.org</a>翻译<br/>
-若你确定该包处于WordPress官方仓库中，则24小时内就会收到该包的翻译推送，若未收到请<a href="https://wp-china.org/forums/forum/104">反馈问题</a>
+<p>
+<strong>该包还未被<a href="https://translate.wp-china.org">本土翻译系统</a>托管哦~</strong>
+</p>
+不要担心，若你确定该包正处于WordPress官方应用市场中，则<a href="https://translate.wp-china.org">本土翻译系统</a>会在24小时内监控到该包并为其提供翻译托管，届时你将收到翻译更新推送，若超过24小时未收到推送，请<a href="https://wp-china.org/forums/forum/104">反馈问题</a><br/>
+再次感谢你对本土化社区的支持^_^
