bcf2ae35bd2b2a62b75701ce661f5c5b1c7deafe
sunxiyuan <sxy@ibadboy.net>
2021-09-05 04:11:32 +0800

    特性：修改“翻译校准”按钮为“参与翻译”，同时更改翻译后端为 litepress.cn 并全局生效

--- wp-china-yes.php
+++ wp-china-yes.php
@@ -36,13 +36,13 @@ if (!class_exists('WP_CHINA_YES')) {
                 /**
                  * 插件列表页中所有插件增加“翻译校准”链接
                  */
-                if (get_option('wpapi') == 1) {
-                    add_filter(sprintf('%splugin_action_links', is_multisite() ? 'network_admin_' : ''), function ($links, $plugin = '') {
-                        $links[] = '<a target="_blank" href="https://translate.wp-china.org/projects/plugins/' . substr($plugin, 0, strpos($plugin, '/')) . '/zh-cn/default">翻译校准</a>';
+               // if (get_option('wpapi') == 1) {
+                 add_filter(sprintf('%splugin_action_links', is_multisite() ? 'network_admin_' : ''), function ($links, $plugin = '') {
+                     $links[] = '<a target="_blank" href="https://litepress.cn/translate/projects/plugins/' . substr($plugin, 0, strpos($plugin, '/')) . '/">参与翻译</a>';
 
-                        return $links;
-                    }, 10, 2);
-                }
+                     return $links;
+                 }, 10, 2);
+                //}
 
 
                 /**
